ccbd0ce79cc9ea422d4ac147192f3f51286485f9
ronaldoroynava <ronaldoroy@dotweb.agency>
2024-03-18 10:37:06 +0100

    done

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const MovieCard = ({ movie}) => {
     return(
-        <div className='movie bg-black rounded-lg hover:cursor-pointer hover:shadow-lg hover:bg-slate-900 hover:scale-105 ease-in-out duration-300'>
+        <div className='movie bg-black rounded-lg hover:cursor-pointer hover:shadow-lg hover:bg-slate-900 hover:scale-105 ease-in-out duration-300 ifhover'>
             <div>
             <p className='absolute bg-black/60 text-white p-2 rounded-br-lg rounded-tl-lg'> {movie.Year}</p>
             </div>
 
             <div>
-                <img className='h-[350px] w-full object-cover rounded-t-lg' src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt="movie1.Title" />
+                <img className='h-[450px] w-full object-cover rounded-lg' src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt="movie1.Title" />
             </div>
-            <div className='p-4'>
+            <div className='p-4 absolute hover:bottom-0 hiddennn bg-slate-950/90 w-full rounded-b-lg'>
                 <span className='text-gray-400'>{movie.Type}</span>
                 <h3 className='text-[20px] text-white  text-container'>{movie.Title}</h3>
             </div>
